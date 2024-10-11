e9ffc6b112c9fee2044c8e5e0d4be6285911db17
Nikhita <nikhitabhatt153@gmail.com>
2024-10-11 21:22:26 +0530

    first commit

---
+++ DevConnector-main/client/src/App.js
@@ -0,0 +1,156 @@
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Dashboard from "./components/dashboard/Dashboard";
+import Alert from "./components/layout/Alert";
+import CreateProfile from "./components/profile-forms/CreateProfile";
+import PrivateRoute from "./components/routing/PrivateRoute";
+import EditProfile from "./components/profile-forms/EditProfile";
+import AddExperience from "./components/profile-forms/AddExperience";
+import AddEducation from "./components/profile-forms/AddEducation";
+import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/Posts";
+import Post from "./components/post/Post";
+//redux
+import { Provider } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
+import "./App.css";
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Landing />} />
+          </Routes>
+          <section className="container">
+            <Alert />
+            <Routes>
+              <Route exact path="/register" element={<Register />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/profiles" element={<Profiles />} />
+              <Route exact path="/profile/:id" element={<Profile />} />
+              <Route
+                exact
+                path="/dashboard"
+                element={
+                  <PrivateRoute>
+                    <Dashboard />
+                  </PrivateRoute>
+                }
+              >
+                <Route exact path="/dashboard" element={<Dashboard />} />
+              </Route>
+
+              <Route
+                exact
+                path="/create-profile"
+                element={
+                  <PrivateRoute>
+                    <CreateProfile />
+                  </PrivateRoute>
+                }
+              >
+                <Route
+                  exact
+                  path="/create-profile"
+                  element={<CreateProfile />}
+                />
+              </Route>
+
+              <Route
+                exact
+                path="/edit-profile"
+                element={
+                  <PrivateRoute>
+                    <EditProfile />
+                  </PrivateRoute>
+                }
+              >
+                <Route exact path="/edit-profile" element={<EditProfile />} />
+              </Route>
+              <Route
+                exact
+                path="/add-experience"
+                element={
+                  <PrivateRoute>
+                    <AddExperience />
+                  </PrivateRoute>
+                }
+              >
+                <Route
+                  exact
+                  path="/add-experience"
+                  element={<AddExperience />}
+                />
+              </Route>
+              <Route
+                exact
+                path="/add-education"
+                element={
+                  <PrivateRoute>
+                    <AddEducation />
+                  </PrivateRoute>
+                }
+              >
+                <Route
+                  exact
+                  path="/add-education"
+                  element={<AddEducation />}
+                />
+              </Route>
+              <Route
+                exact
+                path="/posts"
+                element={
+                  <PrivateRoute>
+                    <Posts />
+                  </PrivateRoute>
+                }
+              >
+                <Route
+                  exact
+                  path="/posts"
+                  element={<Posts />}
+                />
+              </Route>
+              <Route
+                exact
+                path="/posts/:id"
+                element={
+                  <PrivateRoute>
+                    <Post />
+                  </PrivateRoute>
+                }
+              >
+                <Route
+                  exact
+                  path="/posts/:id"
+                  element={<Post />}
+                />
+              </Route>
+            </Routes>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
